71e0c88769d3c4e2d986c73f9bf015ca52ae660b
lokpro <cellurashin@gmail.com>
2021-08-21 17:25:14 +0800

    Update OPR_China_Map_Helper.meta.js

--- Scripts/OPR_China_Map_Helper.meta.js
+++ Scripts/OPR_China_Map_Helper.meta.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         OPR China Map Helper
-// @version      1.0
+// @version      1.2
 // @category     Info
 // @namespace    https://github.com/Ingrass/OPR-Tools/
 // @updateURL    https://github.com/Ingrass/OPR-Tools/raw/master/Scripts/OPR_China_Map_Helper.meta.js
 // @downloadURL  https://github.com/Ingrass/OPR-Tools/raw/master/Scripts/OPR_China_Map_Helper.user.js
 // @description  Add some buttons for China map in OPR
 // @author       Ethern Triomphe346 lokpro 记忆的残骸 stdssr convoi
-// @include      https://wayfarer.nianticlabs.com/*review*
+// @include      https://wayfarer.nianticlabs.com/*
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/prcoords@1.0.0/js/PRCoords.js
-// ==/UserScript==
