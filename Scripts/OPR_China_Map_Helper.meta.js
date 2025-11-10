242048a616d5691930529acd2b70a9d772ab13de
lokpro <2010297+lokpro@users.noreply.github.com>
2025-11-11 01:21:57 +0800

    v1.6 更新網址 opr.ingress.com

--- Scripts/OPR_China_Map_Helper.meta.js
+++ Scripts/OPR_China_Map_Helper.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         OPR China Map Helper
-// @version      1.5
+// @version      1.6
 // @category     Info
 // @namespace    https://github.com/Ingrass/OPR-Tools/
 // @updateURL    https://github.com/Ingrass/OPR-Tools/raw/master/Scripts/OPR_China_Map_Helper.meta.js
@@ -8,6 +8,7 @@
 // @description  Add some buttons for China map in OPR
 // @author       Ethern Triomphe346 lokpro 记忆的残骸 stdssr convoi
 // @include      https://wayfarer.nianticlabs.com/*
+// @include     https://opr.ingress.com/*
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/prcoords@1.0.5/js/PRCoords.min.js
 // ==/UserScript==
